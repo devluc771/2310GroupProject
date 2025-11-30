d9c75dcb8a718ee99224d27519f4395b75b8dcb7
devluc771 <devluc771@gmail.com>
2025-11-29 17:29:39 -0800

    Added SensorData .h and .cpp

---
+++ SensorData.cpp
@@ -0,0 +1,93 @@
+#include "SensorData.h"
+#include <sstream>
+
+SensorData::SensorData() : ts(0), co(0), humidity(0), light(0), lpg(false), motion(0), smoke(0), temp(0) //default constructor where everything is blank
+{
+}
+
+void SensorData::parseRow(string row) //Used ChatGPT to help create parseRow to read a row from the .csv and convert it into a SensorDate object
+{
+    stringstream ss(row);
+    string value;
+
+    // timestamp
+    getline(ss, value, ',');
+    ts = stod(value);
+
+    // device
+    getline(ss, value, ',');
+    device = value;
+
+    // co
+    getline(ss, value, ',');
+    co = stod(value);
+
+    // humidity
+    getline(ss, value, ',');
+    humidity = stod(value);
+
+    // light
+    getline(ss, value, ',');
+    light = stod(value);
+
+    // lpg (0 or 1)
+    getline(ss, value, ',');
+    lpg = (value == "1");
+
+    // motion
+    getline(ss, value, ',');
+    motion = stod(value);
+
+    // smoke
+    getline(ss, value, ',');
+    smoke = stod(value);
+
+    // temp
+    getline(ss, value, ',');
+    temp = stod(value);
+}
+
+long long SensorData::getTs() const
+{
+    return ts;
+}
+
+string SensorData::getDevice() const
+{
+    return device;
+}
+
+double SensorData::getHumidity() const
+{
+    return humidity;
+}
+
+double SensorData::getLight() const
+{
+    return light;
+}
+
+bool SensorData::getLpg() const
+{
+    return lpg;
+}
+
+double SensorData::getMotion() const
+{
+    return motion;
+}
+
+double SensorData::getSmoke() const
+{
+    return smoke;
+}
+
+double SensorData::getTemp() const
+{
+    return temp;
+}
+
+double SensorData::getCo() const
+{
+    return co;
+}
---
+++ SensorData.h
@@ -0,0 +1,32 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+using namespace std;
+#include <string>
+
+class SensorData
+{
+    public:
+        SensorData(); //SensorData Constructor
+        void parseRow(string row);  //takes a line from the CSV and turns it into a SensorData object
+        long long getTs() const;       //accessors
+        string getDevice() const;
+        double getHumidity() const;
+        double getCo() const;
+        double getLight() const;
+        double getMotion() const;
+        double getSmoke() const;
+        double getTemp() const;
+        bool getLpg() const;
+
+    private:
+        long long ts; //timestamp
+        string device; //device ID
+        double co;
+        double humidity;
+        double light;
+        bool lpg;
+        double motion;
+        double smoke;
+        double temp;
+};
+#endif
